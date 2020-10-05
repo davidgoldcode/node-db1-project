b53b7244dd6b5444565156716c554d7949ea0d8e
David Gold <davidgold@hey.com>
2020-10-05 17:50:28 -0400

    finished sql queries

--- queries.sql
+++ queries.sql
@@ -20,9 +20,18 @@ SELECT * FROM Customers
 WHERE City = 'London' OR 'Madrid' OR Country = 'Brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+Insert into Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE CUSTOMERS
+SET PostalCode = '11122'
+WHERE CustomerName = 'The Shire';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT COUNT (DISTINCT City)
+FROM Customers;
+
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT SupplierName FROM Suppliers WHERE length(SupplierName) > 20
